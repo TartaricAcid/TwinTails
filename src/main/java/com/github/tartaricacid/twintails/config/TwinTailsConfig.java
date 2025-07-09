820aa64b5a2da7ed314ab6f3d965c2637e1b15bf
tartaric_acid <baka943@qq.com>
2025-07-09 16:14:36 +0800

    修改生成概率

--- src/main/java/com/github/tartaricacid/twintails/config/TwinTailsConfig.java
+++ src/main/java/com/github/tartaricacid/twintails/config/TwinTailsConfig.java
@@ -8,9 +8,9 @@ public class TwinTailsConfig {
     public static ForgeConfigSpec init() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
-        builder.comment("Chance to add twin tails when monster spawn, default 0.01");
-        builder.comment("怪物生成时添加双马尾的几率，默认 0.01");
-        TWIN_TAILS_ADD_CHANCE = builder.defineInRange("TwinTailsAddChance", 0.01, 0, 1);
+        builder.comment("Chance to add twin tails when monster spawn, default 0.05");
+        builder.comment("怪物生成时添加双马尾的几率，默认 0.05");
+        TWIN_TAILS_ADD_CHANCE = builder.defineInRange("TwinTailsAddChance", 0.05, 0, 1);
 
         return builder.build();
     }
