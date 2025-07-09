0be6d8f31855073fdccf95311f2b6e150efb081a
tartaric_acid <baka943@qq.com>
2025-07-09 16:13:46 +0800

    完成 1.20.1 的同步

--- src/main/java/com/github/tartaricacid/twintails/client/model/ModelTwinTails.java
+++ src/main/java/com/github/tartaricacid/twintails/client/model/ModelTwinTails.java
@@ -41,11 +41,20 @@ public class ModelTwinTails extends EntityModel<Entity> {
     public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         poseStack.pushPose();
 
-        poseStack.mulPose(Vector3f.ZP.rotation(rotationZ));
-        poseStack.mulPose(Vector3f.YP.rotation(rotationY));
-        poseStack.mulPose(Vector3f.XP.rotation(rotationX));
-        poseStack.translate(offsetX, offsetY, offsetZ);
-        poseStack.scale(-scaleX, -scaleY, scaleZ);
+        poseStack.translate(offsetX / 16.0F, offsetY / 16.0F, offsetZ / 16.0F);
+        if (this.rotationZ != 0.0F) {
+            poseStack.mulPose(Vector3f.ZP.rotation(this.rotationZ));
+        }
+        if (this.rotationY != 0.0F) {
+            poseStack.mulPose(Vector3f.YP.rotation(this.rotationY));
+        }
+        if (this.rotationX != 0.0F) {
+            poseStack.mulPose(Vector3f.XP.rotation(this.rotationX));
+        }
+        if (this.scaleX != 1.0F || this.scaleY != 1.0F || this.scaleZ != 1.0F) {
+            poseStack.scale(scaleX, scaleY, scaleZ);
+        }
+        poseStack.scale(-1, -1, 1);
 
         RenderType renderType = Sheets.cutoutBlockSheet();
         MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
